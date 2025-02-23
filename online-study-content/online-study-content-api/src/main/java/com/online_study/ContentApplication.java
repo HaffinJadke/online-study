f5b79bbf247fb7d07694739d011071b06a50ec8a
HaffinJadke <2284789300@qq.com>
2025-02-24 00:14:25 +0800

    2025.2.23(day2)

--- online-study-content/online-study-content-api/src/main/java/com/online_study/ContentApplication.java
+++ online-study-content/online-study-content-api/src/main/java/com/online_study/ContentApplication.java
@@ -3,12 +3,14 @@ package com.online_study;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * 课程管理服务启动类
  */
 @SpringBootApplication
 @EnableSwagger2Doc // 启用Swagger接口文档
+@CrossOrigin //允许跨域
 public class ContentApplication {
 
     public static void main(String[] args) {
